daa57c51bd98535ef10ed0dd4ed644f2984ac23a
Jakub Pinas <jpinas@griddynamics.com>
2025-02-07 14:59:03 +0100

    added  some negative tests

---
+++ src/test/java/trelloWorkflow/TestCreateBoardUnauthorized.java
@@ -0,0 +1,67 @@
+package trelloWorkflow;
+
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
+public class TestCreateBoardUnauthorized extends BaseTest {
+
+    @Test(enabled = true)
+    public static void createBoardWrongKeyAndToken() {
+
+        String wrongKey  = "8554a365f19064aa66123444bebcd230";
+        String wrongToken = "ATTAadbfa99515b6a81298b0f8941133e095ff9d4f915525ed0872e1cb26bcd7fa29F0EBD016";
+        String name            = "Kuba123";
+
+        Response wrongAuth = given().
+                spec(requestSpec).
+                contentType("application/json").
+                queryParam("name", name).
+                queryParam("key",wrongKey).
+                queryParam("token", wrongToken).
+                when().
+                post(); //Send request
+        wrongAuth.prettyPrint();
+
+        Assertions.assertThat(wrongAuth.statusCode()).as("Should be  unauthorized  401").isEqualTo(401);
+    }
+
+    @Test(enabled = true)
+    public static void createBoardWrongKey() {
+
+        String wrongKey  = "8554a365f19064aa66123444bebcd230";
+        String name            = "Kuba123";
+
+        Response wrongAuth = given().
+                spec(requestSpec).
+                contentType("application/json").
+                queryParam("name", name).
+                queryParam("key",wrongKey).
+                queryParam("token", Token).
+                when().
+                post(); //Send request
+        wrongAuth.prettyPrint();
+
+        Assertions.assertThat(wrongAuth.statusCode()).as("Should be  unauthorized  401").isEqualTo(401);
+    }
+
+    @Test(enabled = true)
+    public static void createBoardWrongToken() {
+
+        String wrongToken = "ATTAadbfa99515b6a81298b0f8941133e095ff9d4f915525ed0872e1cb26bcd7fa29F0EBD016";
+        String name            = "Kuba123";
+
+        Response wrongAuth = given().
+                spec(requestSpec).
+                contentType("application/json").
+                queryParam("name", name).
+                queryParam("key",Key).
+                queryParam("token", wrongToken).
+                when().
+                post();
+        wrongAuth.prettyPrint();
+
+        Assertions.assertThat(wrongAuth.statusCode()).as("Should be  unauthorized  401").isEqualTo(401);
+    }
+}
---
+++ src/test/java/trelloWorkflow/TestUpdateBoardWrongRequest.java
@@ -0,0 +1,49 @@
+package trelloWorkflow;
+
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
+public class TestUpdateBoardWrongRequest extends BaseTest{
+
+    @Test
+    public void updateBoardTestWrongUrl() {
+
+
+        System.out.println(BOARD_ID);
+        String endpoint = BOARD_ID + "/lists";
+
+        Response response = given().spec(requestSpec).contentType("application/json").
+                queryParam("name", "123lesgo").
+                queryParam("desc", "siabada").
+                queryParam("prefs/permissionLevel", "public").
+                queryParam("prefs/background", "pink").
+                queryParam("labelNames/green", "spring").
+                when().
+                put(endpoint); //Send request
+        response.prettyPrint();
+        Assert.assertEquals(response.statusCode(),404);
+        System.out.println("404 not found : Provided /{id}/lists instead of /lists/{id}  ");
+    }
+
+    @Test
+    public void updateBoardTestWrongBoardId() {
+
+
+        System.out.println(BOARD_ID);
+        String endpoint = BOARD_ID + "1";
+
+        Response response = given().spec(requestSpec).contentType("application/json").
+                queryParam("name", "123lesgo").
+                queryParam("desc", "siabada").
+                queryParam("prefs/permissionLevel", "public").
+                queryParam("prefs/background", "pink").
+                queryParam("labelNames/green", "spring").
+                when().
+                put(endpoint); //Send request
+        response.prettyPrint();
+        Assert.assertEquals(response.statusCode(),400);
+        System.out.println("400 bad request : Provided wrong  board Id ");
+    }
+}
